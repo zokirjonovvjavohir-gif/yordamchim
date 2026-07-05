7764aa5a7f0c7e3b7e9e2b9cdd5433dd63de09cd
zokirjonovvjavohir-gif <zokirjonovv.javohir@icloud.com>
2026-07-05 14:54:18 +0500

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,20 @@ if not TOKEN or not GROQ_API_KEY:
     exit()
 
 bot = telebot.TeleBot(TOKEN)
+@bot.message_handler(commands=['start'])
+def start(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
 
+    btn1 = types.KeyboardButton("🤖 AI bilan gaplashish")
+    btn2 = types.KeyboardButton("ℹ️ Yordam")
+
+    markup.add(btn1, btn2)
+
+    bot.send_message(
+        message.chat.id,
+        "Salom 🤖 Men AI botman!\nSavol ber yoki tugmani bosing.",
+        reply_markup=markup
+    )
 def ask_ai(text):
     url = "https://api.groq.com/openai/v1/chat/completions"
 
