116a2176c9ac8b8a46360756dbaf73339c2ed661
zokirjonovvjavohir-gif <zokirjonovv.javohir@icloud.com>
2026-07-05 14:11:49 +0500

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def ask_ai(text):
         "Content-Type": "application/json"
     }
     data = {
-    "model": "llama3-8b-8192",
+    ""model": "llama-3.1-8b-instant"
     "messages": [
         {
             "role": "system",
