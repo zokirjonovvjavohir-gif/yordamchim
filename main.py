11804d75b1e07e8281bf14a07ccc382c676a6df3
zokirjonovvjavohir-gif <zokirjonovv.javohir@icloud.com>
2026-07-05 14:20:59 +0500

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def ask_ai(text):
         "Content-Type": "application/json"
     }
     data = {
-     "model": "llama3-8b-8192","messages": [
+     "model": "llama-3.1-8b-instant",,"messages": [
         {
             "role": "system",
             "content": "Sen o‘zbek tilida aniq, tushunarli va qisqa javob beradigan yordamchi botsan."
