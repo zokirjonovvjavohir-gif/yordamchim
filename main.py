298db71b6fc70b3e6fe34829ccf13f049284db73
zokirjonovvjavohir-gif <zokirjonovv.javohir@icloud.com>
2026-07-05 14:53:24 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 import telebot
 import requests
 import os
+from telebot import types
 
+@bot.message_handler(commands=['start'])
+def start(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+
+    btn1 = types.KeyboardButton("🤖 AI bilan gaplashish")
+    btn2 = types.KeyboardButton("ℹ️ Yordam")
+
+    markup.add(btn1, btn2)
+
+    bot.send_message(
+        message.chat.id,
+        "Salom 🤖 Men AI botman!\nSavol ber yoki tugmani bosing.",
+        reply_markup=markup
+    )
 TOKEN = os.getenv("TOKEN")
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
